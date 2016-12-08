ecffc2fb0d3d63a79e370b433743d4afd42196f6
wjcgithub <wjc163@sina.cn>
2016-12-08 08:51:11 +0800

    add demo

---
+++ myfirst_vue_project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1 v-html="title"></h1>
+    <ul>
+      <li v-for="item in items" v-bind:class="{finished: item.isFinished}">
+        {{item.label}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: '<span>span</span>this is title',
+      finished: 'finished',
+      items: [
+        {
+          label: 'coding',
+          isFinished: false
+        },
+        {
+          label: 'walking',
+          isFinished: true
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.finished{
+  text-decoration: underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
